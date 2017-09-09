efbb27e15d360ce352324ccda739bb0b73e5d7e8
David Becker <david@beckersite.com>
2017-09-09 14:23:13 -0600

    api call and authentication working-ish

---
+++ __mock__/userMock.js
@@ -0,0 +1,71 @@
+export default {
+	user: {
+		uid: 'lnZJjAz1mtSjVvzQ8DpXUaMnwIJ3',
+		displayName: 'David Becker',
+		photoURL:
+			'https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/18301003_10155309875982059_1452315781265024827_n.jpg?oh=bdbb3f7b9dc992cbbf3d9877510f093d&oe=5A51AAB0',
+		email: 'david@beckersite.com',
+		emailVerified: false,
+		phoneNumber: null,
+		isAnonymous: false,
+		providerData: [
+			{
+				uid: '10155721376467059',
+				displayName: 'David Becker',
+				photoURL:
+					'https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/18301003_10155309875982059_1452315781265024827_n.jpg?oh=bdbb3f7b9dc992cbbf3d9877510f093d&oe=5A51AAB0',
+				email: 'david@beckersite.com',
+				phoneNumber: null,
+				providerId: 'facebook.com',
+			},
+		],
+		apiKey: 'AIzaSyDIWqOP5S7PwVPmOEIzCCeT0gVnaDhFAuE',
+		appName: '[DEFAULT]',
+		authDomain: 'personal-project-turing-mod3.firebaseapp.com',
+		stsTokenManager: {
+			apiKey: 'AIzaSyDIWqOP5S7PwVPmOEIzCCeT0gVnaDhFAuE',
+			refreshToken:
+				'APRrRCIcfIJRoo69pHyLr3MQFO3y4dcy3eMqDr5J4grKBi0BketbYRq0y3F7VwjFlLVHdqVyl8TP7D6zq7NuJsNrK8LIwOLDcw90oMOnslYadkWjxjJ63FmruUJo_-N-IOvEAxzRbYNgaTSyjUBLCIId-H7v3GolCRKLDeAbBYa_nl3kyoCyMQcwjJGR38m96K5AQR8WwurRDAp3FC8mnIG2hVx8Y3XfdhuUQKF7aP_PHAOZ2Ed1pOsq6b23pvbrVOG2PtNZeW5FnJD-LkBKDHxcyJ7G-O2lE7wqUXiRmJiMMYk7seTJVnFyb_se0nm7wkkxJEcK49oH7_fNl6k4nzBFxAkhIRTfhEmCzQzGfUnG3VLti_wTzEEj9xCQmPgBGvw77N7MFzvG7UHFDLYX4NBlQUcbWrglVcyQt5xbQ-xlBY4x-Dek_qDznpo6D8pws5NFZLOMK4Ok-jOE5dWg4Nc9svK1FEdsfw',
+			accessToken:
+				'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg4ODJlZDY3OTU0MWM5Nzk3ODAzNWM3ZjgzMDQ3MTMzNmU3NGVkY2QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGVyc29uYWwtcHJvamVjdC10dXJpbmctbW9kMyIsIm5hbWUiOiJEYXZpZCBCZWNrZXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9zY29udGVudC54eC5mYmNkbi5uZXQvdi90MS4wLTEvcDEwMHgxMDAvMTgzMDEwMDNfMTAxNTUzMDk4NzU5ODIwNTlfMTQ1MjMxNTc4MTI2NTAyNDgyN19uLmpwZz9vaD1iZGJiM2Y3YjlkYzk5MmNiYmYzZDk4Nzc1MTBmMDkzZCZvZT01QTUxQUFCMCIsImF1ZCI6InBlcnNvbmFsLXByb2plY3QtdHVyaW5nLW1vZDMiLCJhdXRoX3RpbWUiOjE1MDQ5MTc3NDUsInVzZXJfaWQiOiJsblpKakF6MW10U2pWdnpROERwWFVhTW53SUozIiwic3ViIjoibG5aSmpBejFtdFNqVnZ6UThEcFhVYU1ud0lKMyIsImlhdCI6MTUwNDkxNzc0NSwiZXhwIjoxNTA0OTIxMzQ1LCJlbWFpbCI6ImRhdmlkQGJlY2tlcnNpdGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImZhY2Vib29rLmNvbSI6WyIxMDE1NTcyMTM3NjQ2NzA1OSJdLCJlbWFpbCI6WyJkYXZpZEBiZWNrZXJzaXRlLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6ImZhY2Vib29rLmNvbSJ9fQ.f0jB70LH5joX2s5qtpfTtMZj9NKhpNUXkRIN21r8s9zfOmr48o9Ib9FR21WYytXrdcUsesLuvYEsQ_shPSGb91YX6SfTiINDjZ4csISVGafvD2UQ3rnH50dFtxyKBjXFd9msiBeqDiVdSGoq_BnTPUe0Cu8TOqVl9eVVGFgNZv4YKm2dS-cV7jjqJmk7hvjmPBWF-1zEj4yOXDlnW4_NlcA2jUkJH4jmDMHXsb5zOd5yJnOLKFCxXttOHLEYTkbxuHq2lTtkIN9WiUqRvUYVfDkydO-jN7juSYhCEDmSmQqKOUTsQcOPuIWNaeR8u67k6Md3MWjvxDp51fuGGYAJfg',
+			expirationTime: 1504921345230,
+		},
+		redirectEventId: null,
+	},
+	credential: {
+		accessToken:
+			'EAALIkHRIM1oBALcxejZAVnGJF7UGOUYYhxRhf4ZBTNK9NcX9pkbLBsveX75LNQv1rFQdNMgf7UFEUZBSGvWOVNdYGNQ4PjH3nyFs3GPuZA71mslG9kQpZB1xf6Kyx2vOwpnUnGHTNZC6SIaJUYVvKuRjT2ZC0aOmgoZD',
+		providerId: 'facebook.com',
+	},
+	additionalUserInfo: {
+		profile: {
+			updated_time: '2017-05-08T05:57:44+0000',
+			gender: 'male',
+			timezone: -6,
+			link: 'https://www.facebook.com/app_scoped_user_id/10155721376467059/',
+			verified: true,
+			last_name: 'Becker',
+			locale: 'en_US',
+			picture: {
+				data: {
+					is_silhouette: false,
+					url:
+						'https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/18301003_10155309875982059_1452315781265024827_n.jpg?oh=bdbb3f7b9dc992cbbf3d9877510f093d&oe=5A51AAB0',
+				},
+			},
+			age_range: {
+				min: 21,
+			},
+			name: 'David Becker',
+			location: {
+				name: 'Denver, Colorado',
+				id: '115590505119035',
+			},
+			id: '10155721376467059',
+			first_name: 'David',
+			email: 'david@beckersite.com',
+		},
+		providerId: 'facebook.com',
+	},
+	operationType: 'signIn',
+};
