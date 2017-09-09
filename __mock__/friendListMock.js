8a2cf315831f485b73c55e928cb0992d3912c472
David Becker <david@beckersite.com>
2017-09-09 15:20:24 -0600

    friends displaying on page as list

--- __mock__/friendListMock.js
+++ __mock__/friendListMock.js
@@ -1,3 +1,83 @@
 export default {
-  
-}
+	friends: {
+		data: [
+			{
+				name: 'Azeem Sola',
+				id: '506836739',
+			},
+			{
+				name: 'Forrest Sansing',
+				id: '562272102',
+			},
+			{
+				name: 'Kevin Shutzberg',
+				id: '672522675',
+			},
+			{
+				name: 'Rohan Jhunjhunwala',
+				id: '711184623',
+			},
+			{
+				name: 'Adam Bloom',
+				id: '785658285',
+			},
+			{
+				name: 'Jacob Long',
+				id: '10207829234506075',
+			},
+			{
+				name: 'Daniel Holmberg Nielsen',
+				id: '10206040446110163',
+			},
+			{
+				name: 'Tristan McCormick',
+				id: '10205941521038849',
+			},
+			{
+				name: 'Dominique Saks',
+				id: '1361163216',
+			},
+			{
+				name: 'Judd Plaza Rosenblatt',
+				id: '1374330331',
+			},
+			{
+				name: 'Michael Kauzmann',
+				id: '10206781479481905',
+			},
+			{
+				name: 'Sam Goldstein',
+				id: '1511070063',
+			},
+			{
+				name: 'David W Grossman',
+				id: '1062800187109831',
+			},
+			{
+				name: 'Sara Mae',
+				id: '798624880194307',
+			},
+		],
+		paging: {
+			cursors: {
+				before:
+					'QVFIUlhVMi1YUkt4anE5WVRaYmY4UTlZAZA2kwdUE4T0Y1bDJwa2hNdGcyT1JhTllCcUJCbS1DZAThod2thNE5XTkt2eGQZD',
+				after:
+					'QVFIUnVHdEhtOXBlNzZAZAOUEtbmI4V0tDaHZAZAYkpVTm5rV3JWYkVMcy1zTkhHYmljRU8yMVFrRnQ0ZA3VTMVl6bnM2UFFNOE1ISFYwQ2tHOHlOT2NXRjNrc2ln',
+			},
+		},
+		summary: {
+			total_count: 1760,
+		},
+	},
+	hometown: {
+		id: '103123839728353',
+		name: 'Memphis, Tennessee',
+	},
+	location: {
+		id: '115590505119035',
+		name: 'Denver, Colorado',
+	},
+	name: 'David Becker',
+	id: '10152786482452059',
+};
